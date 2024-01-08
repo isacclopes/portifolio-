552001b99296d823a452b5f180377c4947c531be
isacclopes <156002815+isacclopes@users.noreply.github.com>
2024-01-08 17:32:54 -0300

    subir os arquivos do meu site

---
+++ main.js
@@ -0,0 +1,4 @@
+// Função para redirecionar ao clicar no botão
+document.getElementById("redirecionar").addEventListener("click", function() {
+    window.location.href = "./index.html"; // link de destino
+});
---
+++ pizza/pizzaDoc.js
@@ -0,0 +1,39 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    const img = document.querySelector('.delivery img');
+    let currentPosition = 50; // Posição horizontal atual da imagem
+    const speed = 25; // Velocidade do movimento (ajuste conforme necessário)
+    const maxDistance = 850; // Distância máxima à direita
+
+    window.addEventListener('wheel', function (event) {
+        // Calcula a direção do movimento da roda do mouse
+        const scrollDirection = event.deltaY > 0 ? 1 : -1;
+
+        // Calcula a nova posição horizontal da imagem
+        currentPosition += speed * scrollDirection;
+
+        // Limita a imagem a uma margem máxima
+        currentPosition = Math.min(currentPosition, maxDistance);
+
+        // Move a imagem para a nova posição
+        img.style.transform = `translateX(${currentPosition}px)`;
+    });
+});
+
+///parte do alert para responsividade da pagina 
+
+document.addEventListener('DOMContentLoaded', function () {
+    function showAlert() {
+        alert('Somente desktop');
+    }
+
+    const screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+
+    if (screenWidth <= 726) {
+        showAlert();
+    }
+
+    document.getElementById('fecharAlerta').addEventListener('click', function () {
+        showAlert();
+    });
+});
